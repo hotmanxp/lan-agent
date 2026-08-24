8238f4e92e4d162a534f23eac10533ca6cb8dcc0
Ethan <6923102@qq.com>
2026-08-24 22:44:58 +0800

    chore: bump version to 0.1.1 / versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.hotmanxp.lanagent"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
